c5753d069d412decd47afcfcad3be391dafa8223
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-09 13:06:26 -0300

    Finishing implementation of all proj dependencies

---
+++ WebApp_Monitoring/MonitoringSite/Models/MonitoringDependency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringSite.Models
+{
+    public class MonitoringDependency
+    {
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+        public string Url { get; set; }
+        public string UriEndpoint { get; set; }
+        public string PrimaryKey { get; set; }
+        public string QueueName { get; set; }
+    }
+}
--- WebApp_Monitoring/MonitoringSite/Startup.cs
+++ WebApp_Monitoring/MonitoringSite/Startup.cs
@@ -1,8 +1,20 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MonitoringSite.Extensions;
+using MonitoringSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 
 namespace MonitoringSite
 {
@@ -15,13 +27,22 @@ namespace MonitoringSite
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            var dadosDependencias = new List<MonitoringDependency>();
+
+            new ConfigureFromConfigurationOptions<List<MonitoringDependency>>(
+                Configuration.GetSection("Dependencies"))
+                    .Configure(dadosDependencias);
+            dadosDependencias = dadosDependencias.OrderBy(d => d.Name).ToList();
+
+            // Verificando a disponibilidade dos bancos de dados
+            // da aplicação através de Health Checks
+            services.AddHealthChecks()
+                .AddDependencies(dadosDependencias);
+            services.AddHealthChecksUI();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -29,16 +50,34 @@ namespace MonitoringSite
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            // Gera o endpoint que retornará os dados utilizados no dashboard
+            app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
             {
-                endpoints.MapControllers();
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI();
+
+            // Endpoint para retorno de informações sobre Health Checks
+            // a serem utilizados pelo Worker Process de Monitoramento
+            app.UseHealthChecks("/status-monitoramento",
+               new HealthCheckOptions()
+               {
+                   ResponseWriter = async (context, report) =>
+                   {
+                       var result = JsonSerializer.Serialize(
+                            report.Entries.Select(e => new
+                            {
+                                healthCheck = e.Key,
+                                error = e.Value.Exception?.Message,
+                                status = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                            }));
+                       context.Response.ContentType = MediaTypeNames.Application.Json;
+                       await context.Response.WriteAsync(result);
+                   }
+               });
         }
     }
 }
