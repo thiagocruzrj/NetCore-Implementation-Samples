e0edf13414c32afa1747a7b798035ad49c551064
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-26 15:07:45 -0300

    Creating RabbitMQ entity configuration on RabbitMQ_messages solution

---
+++ RabbitMQ_Messages/Configuration/RabbitMQEntityConfiguration.cs
@@ -0,0 +1,10 @@
+namespace RabbitMQ_Messages.Configuration
+{
+    public class RabbitMQEntityConfiguration
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
