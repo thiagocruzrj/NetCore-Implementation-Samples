f0d18a64a57d96fddcd49054d948cb9d5d3204b5
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-26 15:28:32 -0300

    Implementing Post method on messagesController from RabbitMQ_messages solution

--- RabbitMQ_Messages/Controller/MessagesController.cs
+++ RabbitMQ_Messages/Controller/MessagesController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ_Messages.Configuration;
 using RabbitMQ_Messages.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RabbitMQ_Messages.Controller
@@ -20,6 +23,51 @@ namespace RabbitMQ_Messages.Controller
             {
                 QtMessageSent = _Conter.ActualValue
             };
+        }
+
+        [HttpPost]
+        public object Post(
+            [FromServices]RabbitMQEntityConfiguration configurations,
+            [FromBody]Content content)
+        {
+            lock (_Conter)
+            {
+                _Conter.Increment();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = configurations.HostName,
+                    Port = configurations.Port,
+                    UserName = configurations.UserName,
+                    Password = configurations.Password
+                };
+
+                using (var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "TestAspCore",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message =
+                        $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
+                        $"Message content: {content.Message}";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "TestAspCore",
+                                         basicProperties: null,
+                                         body: body);
+                }
+
+                return new
+                {
+                    Result = "Message sent with sucess"
+                };
+            }
         }
     }
 }
