8b65c7c3b766841f713b8ef1d6b364ce89bf041f
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-26 15:20:29 -0300

    Implementing GET method on messagesController from RabbitMQ_messages solution

---
+++ RabbitMQ_Messages/Controller/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ_Messages.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_Messages.Controller
+{
+    [ApiController]
+    [Route("Controller")]
+    public class MessagesController : ControllerBase
+    {
+        private static Counter _Conter = new Counter();
+
+        [HttpGet]
+        public object Get()
+        {
+            return new
+            {
+                QtMessageSent = _Conter.ActualValue
+            };
+        }
+    }
+}
