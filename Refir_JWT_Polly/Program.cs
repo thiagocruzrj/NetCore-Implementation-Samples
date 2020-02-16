1a476db7b1909d4e62c250b4a9d60a36e8bc18d1
Thiago Cruz <thagocruz@gmail.com>
2020-02-15 22:50:56 -0300

    Starting implementation of fails tratament with polly and refit

---
+++ Refir_JWT_Polly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refir_JWT_Polly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
