4a8172ec530f248e9ee94ad33a62c9e0976184ba
Thiago Cruz <thagocruz@gmail.com>
2020-02-15 23:04:54 -0300

    Implement all interfaces

---
+++ Refir_JWT_Polly/Interfaces/ILoginApi.cs
@@ -0,0 +1,12 @@
+﻿using Refir_JWT_Polly.Models;
+using Refit;
+using System.Threading.Tasks;
+
+namespace Refir_JWT_Polly.Interfaces
+{
+    public interface ILoginApi
+    {
+        [Post("/login")]
+        Task<Token> PostCredentials(User user);
+    }
+}
---
+++ Refir_JWT_Polly/Interfaces/IProductsApi.cs
@@ -0,0 +1,21 @@
+﻿using Refir_JWT_Polly.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refir_JWT_Polly.Interfaces
+{
+    public interface IProductsApi
+    {
+        [Get("/Products")]
+        Task<List<Product>> ListProducts(
+            [Header("Authorization")] string token);
+
+        [Post("/Product")]
+        Task<ApiProductsResult> IncludeProduct(
+            [Header("Authorization")] string token,
+            [Body]Product product);
+    }
+}
