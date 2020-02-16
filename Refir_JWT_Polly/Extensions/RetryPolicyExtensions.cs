6e94fb3e5eb73bb9eb588e42baf4a17a6225c410
Thiago Cruz <thagocruz@gmail.com>
2020-02-15 23:13:01 -0300

    Implement retry policy

---
+++ Refir_JWT_Polly/Extensions/RetryPolicyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.Retry;
+using Refir_JWT_Polly.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Refir_JWT_Polly.Extensions
+{
+    public static class RetryPolicyExtensions
+    {
+        public static Task<T> ExecuteWithTokenAsync<T>(
+            this AsyncRetryPolicy retryPolicy,
+            Token token,
+            Func<Context, Task<T>> action)
+        {
+            return retryPolicy.ExecuteAsync(action, new Dictionary<string, object>
+            {
+                {"AccessToken", token.AccessToken }
+            });
+        }
+    }
+}
