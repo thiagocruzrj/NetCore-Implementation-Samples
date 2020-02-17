446276382411e5c5a0e6fdd6babeb2fe8f672c8f
Thiago Cruz <thagocruz@gmail.com>
2020-02-16 23:54:20 -0300

    Creating Include and authenticate methods

--- Refir_JWT_Polly/Clients/ProductClientApi.cs
+++ Refir_JWT_Polly/Clients/ProductClientApi.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Polly;
 using Polly.Retry;
+using Refir_JWT_Polly.Extensions;
 using Refir_JWT_Polly.Interfaces;
 using Refir_JWT_Polly.Models;
 using Refit;
 using System;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Refir_JWT_Polly.Clients
 {
@@ -25,6 +29,37 @@ namespace Refir_JWT_Polly.Clients
             _loginApi = RestService.For<ILoginApi>(urlBase);
             _productsApi = RestService.For<IProductsApi>(urlBase);
             _jwtPolicy = CreateAccessTokenPolicy();
+        }
+
+        public Task Authenticate()
+        {
+            try
+            {
+                _token = _loginApi.PostCredentials(
+                    new User()
+                    {
+                        UserId = _configuration.GetSection("ApiProducts_Access:UserId").Value,
+                        Password = _configuration.GetSection("ApiProducts_Access:Password").Value
+                    }).Result;
+            }
+            catch
+            {
+                _token = null;
+                return Console.Out.WriteLineAsync("Fail to authenticate ...");
+            }
+        }
+
+        public Task IncludeProduct(Product product)
+        {
+            var inclusion = _jwtPolicy.ExecuteWithTokenAsync<ApiProductsResult>(
+                _token, async (Context) =>
+                {
+                    var result = await _productsApi.IncludeProduct(
+                        $"Bearer {Context["AccessToken"]}", product);
+
+                    return result;
+                });
+            return Console.Out.WriteLineAsync(JsonSerializer.Serialize(inclusion.Result));
         }
 
         private AsyncRetryPolicy CreateAccessTokenPolicy()
